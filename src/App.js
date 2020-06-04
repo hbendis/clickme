ceef519b9208830ccb7c6b9664e1414cee3f1cc8
hbendis <bendishoxha1@gmail.com>
2020-06-04 14:16:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Application extends Component {
+constructor(props){
+    super(props);
+this.state= {
+    count:0
 }
 
-export default App;
+}
+
+handleClick = () => {
+this.setState({count: this.state.count +1})};
+
+
+render() {
+    let {count} = this.state;
+
+return (
+    <div>
+    <h1>You clicked the button {count} times</h1>
+    <span>
+        
+    <button onClick={() => this.handleClick()}>Click Me</button>
+    </span>
+    </div>
+
+);
+}
+
+}
+
+
+export default Application;
