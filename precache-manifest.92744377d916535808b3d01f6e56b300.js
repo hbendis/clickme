617a127876c2d0e05b0969bc6231c12b94febd13
hbendis <bendishoxha1@gmail.com>
2020-06-04 14:17:12 +0200

    Updates

---
+++ precache-manifest.92744377d916535808b3d01f6e56b300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576d309dd90ed0428b54346cbeedbdf2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f87dc309a92432d6bfe7",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "458b2ee18c0fd92b4026",
+    "url": "./static/js/2.a4d80391.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a4d80391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87dc309a92432d6bfe7",
+    "url": "./static/js/main.cce41ce9.chunk.js"
+  },
+  {
+    "revision": "f61d21b5372d3b39cd89",
+    "url": "./static/js/runtime-main.58887af7.js"
+  }
+]);
